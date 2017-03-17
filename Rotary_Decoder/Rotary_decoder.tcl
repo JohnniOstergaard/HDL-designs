a8c0a79b729b26d16ffb098e2f946c7cfbd08208
Johnni Østergaard <johnni.ostergaard@gmail.com>
2017-03-17 11:05:00 +0100

    Added Rotary_Decoder.Tcl

--- Rotary_Decoder/Rotary_decoder.tcl
+++ Rotary_Decoder/Rotary_decoder.tcl
@@ -2,7 +2,7 @@
 #   Test script for the TB_Rotary_decoder testbench.
 #Information ===================================================================
 #   File name:      Rotary_decoder.Tcl
-#   Target file:    TB_Rotary_decoder.VHDL
+# 	 Target file:	  TB_Rotary_decoder.VHDL
 #   Engineer:       Johnni Østergaard
 #   Copyright:      (c) 2017 Johnni Østergaard
 #   Credits:         
@@ -16,22 +16,22 @@
 #===============================================================================
 
 #Delet old library files
-if {[file exists work]} then {vdel -lib work -all}
+if {[file exists work]} 	  then {vdel -lib work -all}
 
 #Waveforms-----(Colors: green, cyan, orange, yellow or orchid)--------------------
 add wave -divider Controls
-add wave -color green	-binary 	-position insertpoint \sim:/TB_Rotary_decoder/uut/Clk
-add wave -color green	-binary 	-position insertpoint \sim:/TB_Rotary_decoder/uut/Rst
-add wave -color green	-binary 	-position insertpoint \sim:/TB_Rotary_decoder/uut/Mode
+add wave -color green	-binary 		-position insertpoint \sim:/TB_Rotary_decoder/uut/Clk
+add wave -color green	-binary 		-position insertpoint \sim:/TB_Rotary_decoder/uut/Rst
+add wave -color green	-binary 		-position insertpoint \sim:/TB_Rotary_decoder/uut/Mode
 
 add wave -divider Signals
 add wave -color orchid 	-binary		-position insertpoint \sim:/TB_Rotary_decoder/uut/Sensor
 add wave -color orchid 	-binary		-position insertpoint \sim:/TB_Rotary_decoder/uut/Past
-#add wave -color orchid -binary		-position insertpoint \sim:/TB_Rotary_decoder/uut/Rotary_Q1
-#add wave -color orchid -binary		-position insertpoint \sim:/TB_Rotary_decoder/uut/Rotary_Q2
-#add wave -color orchid -binary		-position insertpoint \sim:/TB_Rotary_decoder/uut/Rotary_Q1_Reg
-#add wave -color orchid -Unsigned	-position insertpoint \sim:/TB_Rotary_decoder/uut/Z_Reg
-#add wave -color orchid -Unsigned	-position insertpoint \sim:/TB_Rotary_decoder/uut/Rot_temp
+#add wave -color orchid 	-binary		-position insertpoint \sim:/TB_Rotary_decoder/uut/Rotary_Q1
+#add wave -color orchid 	-binary		-position insertpoint \sim:/TB_Rotary_decoder/uut/Rotary_Q2
+#add wave -color orchid 	-binary		-position insertpoint \sim:/TB_Rotary_decoder/uut/Rotary_Q1_Reg
+#add wave -color orchid 	-Unsigned	-position insertpoint \sim:/TB_Rotary_decoder/uut/Z_Reg
+#add wave -color orchid 	-Unsigned	-position insertpoint \sim:/TB_Rotary_decoder/uut/Rot_temp
 	
 add wave -divider Inputs
 add wave -color cyan 	-binary   	-position insertpoint \sim:/TB_Rotary_decoder/uut/A
@@ -44,7 +44,7 @@ add wave -color orange  -binary		-position insertpoint \sim:/TB_Rotary_decoder/u
 add wave -color orange  -binary		-position insertpoint \sim:/TB_Rotary_decoder/uut/Rot
 
 add wave -divider Test_bench
-add wave -color yellow              	-position insertpoint \sim:/TB_Rotary_decoder/Test_case
+add wave -color yellow              -position insertpoint \sim:/TB_Rotary_decoder/Test_case
 
 #Simulation time/window-----(Time units: ms, us, ns)-------------------------------
 run 50us
