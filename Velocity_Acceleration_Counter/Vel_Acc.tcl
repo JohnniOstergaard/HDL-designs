4be42281764327e0fe7c37e1e9a9b47290dd7ab4
Johnni Østergaard <johnni.ostergaard@gmail.com>
2017-03-23 10:14:37 +0100

    Added Vel_Reg & updated Info text

--- Velocity_Acceleration_Counter/Vel_Acc.tcl
+++ Velocity_Acceleration_Counter/Vel_Acc.tcl
@@ -1,13 +1,19 @@
-#Info-----------------------------------------------------------------------------
-# File name:	  Vel_Acc.tcl
-# Target file:	  TB_Vel_Acc.VHDL
-# Description: 	  Test script for the Vel_Acc testbench.
-#
-# Tool versions:  ModelSim Altera starter edition 10.4d
-# Engineer:	  Johnni Østergaard
-# Create date: 	  20.03.2017
-# Revision:	  1.00 - Base functionality validted in testbench
-#---------------------------------------------------------------------------------
+#Description ===================================================================
+#   Test script for the TB_Vel_Acc testbench.
+#Information ===================================================================
+#   File name:      Vel_Acc.tcl
+# 	 Target file:	  TB_Vel_Acc.VHDL
+#   Engineer:       Johnni Østergaard
+#   Copyright:      (c) 2017 Johnni Østergaard
+#   Credits:         
+#   License:        MIT License
+#   Interpreter:    ModelSim Altera starter edition 10.4d
+#Progress ======================================================================
+#   Status:         Development
+#   Version:        1.0.0        | Major.minor.patch
+#   Created:        20.03.2017
+#   Modified:       23-03-2017   | 
+#===============================================================================
 
 #Delet old library files
 if {[file exists work]} then {vdel -lib work -all}
@@ -19,6 +25,7 @@ add wave -color green	-binary 	-position insertpoint \sim:/TB_Vel_Acc/uut/Rst
 
 add wave -divider Signals
 add wave -color orchid 	-Unsigned	-position insertpoint \sim:/TB_Vel_Acc/uut/Clk_count
+add wave -color orchid 	-decimal	-position insertpoint \sim:/TB_Vel_Acc/uut/Vel_Reg
 add wave -color orchid 	-decimal	-position insertpoint \sim:/TB_Vel_Acc/uut/Vel_sample
 add wave -color orchid 	-decimal	-position insertpoint \sim:/TB_Vel_Acc/uut/Vel_sample2
 add wave -color orchid 	-binary		-position insertpoint \sim:/TB_Vel_Acc/uut/Pulse1_set
@@ -33,7 +40,7 @@ add wave -color orange  -decimal	-position insertpoint \sim:/TB_Vel_Acc/uut/Vel
 add wave -color orange  -decimal	-position insertpoint \sim:/TB_Vel_Acc/uut/Acc 
 
 add wave -divider Test_bench
-add wave -color yellow  		-position insertpoint \sim:/TB_Vel_Acc/Test_case
+add wave -color yellow              	-position insertpoint \sim:/TB_Vel_Acc/Test_case
 
 #Simulation time/window-----(Time units: ms, us, ns)-------------------------------
 run 1500us
