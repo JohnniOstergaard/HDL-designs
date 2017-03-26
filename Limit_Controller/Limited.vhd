737498c7bdfe101330930bc2fe148f131a2c814c
Johnni Østergaard <johnni.ostergaard@gmail.com>
2017-03-26 23:47:45 +0200

    Updated text format

--- Limit_Controller/Limited.vhd
+++ Limit_Controller/Limited.vhd
@@ -6,11 +6,11 @@
 --   Copyright:      (c) 2017 Johnni Østergaard
 --   Credits:         
 --   License:        MIT License
---   Compatibility:	VHDL-2008
+--   Compatibility:  VHDL-2008
 --Progress ======================================================================
 --   Status:         Development
 --   Version:        1.0.2        | Major.minor.patch
---   Created:       	13-10-2016
+--   Created:        13-10-2016
 --   Modified:       26-03-2017   | Updated the information text
 --===============================================================================
 
@@ -23,22 +23,21 @@ library ieee;
 
 entity Limited is
 	generic( Bit_width :positive := 8 );																--Channel width in bits
-	port( Clk  	  :in  std_logic;																			--System clock
-			Rst  	  :in  std_logic;																			--Reset logic
-			Max     :in  std_logic;																			--End stop at maximum position 
-			Min     :in  std_logic;																			--End stop at minimum position 
-			Pre_dir :in  std_logic;																			--Preset direction
-			Dir  	  :in  std_logic;																			--Direction input
-			Duty 	  :in  std_logic_vector(Bit_width-1 downto 0);									--Strength  input
-			Q_Dir   :out std_logic := '0';																--Direction output response 
-			Q_Duty  :out std_logic_vector(Bit_width-1 downto 0) := (others => '0') );		--Strength  output response 
+	port( Clk     :in  std_logic;																			--System clock
+	      Rst     :in  std_logic;																			--Reset logic
+	      Max     :in  std_logic;																			--End stop at maximum position 
+	      Min     :in  std_logic;																			--End stop at minimum position 
+	      Pre_dir :in  std_logic;																			--Preset direction
+	      Dir     :in  std_logic;																			--Direction input
+	      Duty    :in  std_logic_vector(Bit_width-1 downto 0);									--Strength  input
+	      Q_Dir   :out std_logic := '0';																--Direction output response 
+	      Q_Duty  :out std_logic_vector(Bit_width-1 downto 0) := (others => '0') );		--Strength  output response 
 end Limited;
 
-architecture Behavioral of Limited is
-		
+architecture Behavioral of Limited is	
 	begin
 	process(Clk, Rst) is
-	begin
+		begin
 		if(Rst = '0') then
 			Q_Dir  <= '0';
 			Q_Duty <= (others => '0');
