ef063d06b0dd745ab076792300f1ec81bf0e1eaf
Johnni Østergaard <johnni.ostergaard@gmail.com>
2017-03-26 23:42:59 +0200

    Added Limited.Tcl and Limited.VHDL

---
+++ Limit_Controller/Limited.tcl
@@ -0,0 +1,42 @@
+#Description ===================================================================
+#   Test script for the TB_Limited testbench.
+#Information ===================================================================
+#   File name:      Limited.tcl
+# 	 Target file:	  TB_Limited.VHDL
+#   Engineer:       Johnni Østergaard
+#   Copyright:      (c) 2017 Johnni Østergaard
+#   Credits:         
+#   License:        MIT License
+#   Interpreter:    ModelSim Altera starter edition 10.4d
+#Progress ======================================================================
+#   Status:         Development
+#   Version:        1.0.0        | Major.minor.patch
+#   Created:        13.10.2016
+#   Modified:       26-03-2017   | Added the Pre_dir signal
+#===============================================================================
+
+#Delet old library files
+if {[file exists work]} then {vdel -lib work -all}
+
+#Signals-----(Colors: green, cyan, orange, yellow or orchid)--------------------	
+add wave -divider Controls
+add wave -color green	-binary 		-position insertpoint \sim:/TB_Limited/uut/Clk
+add wave -color green	-binary 		-position insertpoint \sim:/TB_Limited/uut/Rst
+
+add wave -divider Inputs
+add wave -color cyan 	-binary		-position insertpoint \sim:/TB_Limited/uut/Max
+add wave -color cyan 	-binary		-position insertpoint \sim:/TB_Limited/uut/Min
+add wave -color cyan 	-binary		-position insertpoint \sim:/TB_Limited/uut/Pre_dir
+add wave -color cyan		-binary		-position insertpoint \sim:/TB_Limited/uut/Dir
+add wave -color cyan		-unsigned	-position insertpoint \sim:/TB_Limited/uut/Duty
+
+add wave -divider Outputs
+add wave -color orange	-binary		-position insertpoint \sim:/TB_Limited/uut/Q_Dir
+add wave -color orange	-unsigned	-position insertpoint \sim:/TB_Limited/uut/Q_Duty
+
+add wave -divider Test_bench
+add wave -color yellow              -position insertpoint \sim:/TB_Limited/Test_case
+
+#Simulation time/window-----(Time units: ms, us, ns)-------------------------------
+run 1000ns
+wave zoom range 0ns 850ns
---
+++ Limit_Controller/Limited.vhd
@@ -0,0 +1,57 @@
+--Description ===================================================================
+--   End stop limited.
+--Information ===================================================================
+--   File name:      Limited.VHDL
+--   Engineer:       Johnni Østergaard
+--   Copyright:      (c) 2017 Johnni Østergaard
+--   Credits:         
+--   License:        MIT License
+--   Compatibility:	VHDL-2008
+--Progress ======================================================================
+--   Status:         Development
+--   Version:        1.0.2        | Major.minor.patch
+--   Created:       	13-10-2016
+--   Modified:       26-03-2017   | Updated the information text
+--===============================================================================
+
+--Including------------------------
+library ieee;
+	use ieee.std_logic_1164.all;
+	use ieee.std_logic_unsigned.all;
+	use ieee.numeric_std.all;
+-----------------------------------
+
+entity Limited is
+	generic( Bit_width :positive := 8 );																--Channel width in bits
+	port( Clk  	  :in  std_logic;																			--System clock
+			Rst  	  :in  std_logic;																			--Reset logic
+			Max     :in  std_logic;																			--End stop at maximum position 
+			Min     :in  std_logic;																			--End stop at minimum position 
+			Pre_dir :in  std_logic;																			--Preset direction
+			Dir  	  :in  std_logic;																			--Direction input
+			Duty 	  :in  std_logic_vector(Bit_width-1 downto 0);									--Strength  input
+			Q_Dir   :out std_logic := '0';																--Direction output response 
+			Q_Duty  :out std_logic_vector(Bit_width-1 downto 0) := (others => '0') );		--Strength  output response 
+end Limited;
+
+architecture Behavioral of Limited is
+		
+	begin
+	process(Clk, Rst) is
+	begin
+		if(Rst = '0') then
+			Q_Dir  <= '0';
+			Q_Duty <= (others => '0');
+			
+		elsif rising_edge(Clk) then
+			Q_Dir  <= Dir;
+			if((Max = '0') and (Dir = Pre_dir)) then
+				Q_Duty <= (others => '0');									--Set response strength to zero
+			elsif((Min = '0') and (Dir = not Pre_dir)) then
+				Q_Duty <= (others => '0');									--Set response strength to zero
+			else
+				Q_Duty <= Duty;												--Set duty to the rest of the Re vector
+			end if;
+		end if;
+	end process;
+end Behavioral;
